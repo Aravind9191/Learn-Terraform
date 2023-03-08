4ed2aa884d49367f6a3c3fa297ab7991e0abd613
Aravind Leela <aravindleela.a@gmail.com>
2023-03-08 18:56:38 +0530

    prometheus

---
+++ prometheus-test-servers/main.tf
@@ -0,0 +1,20 @@
+resource "aws_spot_instance_request" "node1" {
+  ami           = "ami-09a94b38daf9069c5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sgr-083fcfc7839001cbd	"]
+  wait_for_fulfillment =  true
+
+  tags = {
+    Name = "prom-test-server"
+  }
+}
+
+resource "aws_spot_instance_request" "node2" {
+  ami           = "ami-09a94b38daf9069c5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sgr-083fcfc7839001cbd	"]
+
+  tags = {
+    Name = "prom-test-server"
+  }
+}
